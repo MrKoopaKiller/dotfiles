d3f7791c85113a0610b07dc57c2ec5883910775b
Raphael Rabello <raphael@rabello.me>
2025-08-06 01:10:15 -0300

    fix install script checks

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -u
 
 brew_install() {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval $(/opt/homebrew/bin/brew shellenv)
-    BREW=(which brew)
+    BREW=$(which brew)
 }
 
 logging() {
     echo "$(date -u +'%Y-%m-%d:%H:%M:%S UTC') - Installed" >> install.log
-    touch $(date) > installed.lock
+    date -u > installed.lock
 }
 
 BREW="/opt/homebrew/bin/brew"
 DOTDIR="${HOME}/dotfiles"
 [ ! -f $DOTDIR/install.sh ] && git clone https://github.com/MrKoopaKiller/dotfiles.git $DOTDIR
 
-if [[ -f "$BREW" ]]; then brew_install; fi
+if [[ ! -f "$BREW" ]]; then brew_install; fi
 if [[ $(uname) == 'Darwin' ]]; then brew bundle install; fi
-if [[ $(uname) == 'linux' ]]; then xargs sudo apt-get -y install < ${DOTDIR}/aptfile; fi
+if [[ $(uname) == 'Linux' ]]; then xargs sudo apt-get -y install < ${DOTDIR}/aptfile; fi
 
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
